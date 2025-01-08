e90d1221b3ff9159f93f6dcb11aee82b29cef959
fuad46 <fuadalzabir5@gmail.com>
2025-01-08 20:06:48 +0600

    2nd commit

--- store/views.py
+++ store/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .models import Product
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .forms import SignUpForm
+from django import forms
 
 # Create your views here.
 def home(request):
@@ -33,3 +37,22 @@ def logout_user(request):
     logout(request)
     messages.success(request, ("You Are Logged out"))
     return redirect('home')
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Account Create Succesful."))
+            return redirect('home')
+        else:
+            messages.success(request, ("Account Create Failed."))
+            return redirect('register')
+
+    else:
+        return render(request, 'register.html', {'form':form})
